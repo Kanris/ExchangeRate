e817e845f5e64516cca4df31db239bffcee209d3
Alexey Glukhov <kanris2@gmail.com>
2018-02-10 17:58:24 +0200

    Add constructor to BankInfo class

--- ExchangeRateLibrary/BankInfo.cs
+++ ExchangeRateLibrary/BankInfo.cs
@@ -14,11 +14,20 @@ namespace ExchangeRateLibrary
 
         public string URI { set; get; } //WebSite address
 
-        public string pattern { set; get; } //"html line" - where to get exchange value
+        public string Pattern { set; get; } //"html line" - where to get exchange value
 
         public int IndexBuy { set; get; } = 0; //which Regex.Match corresponds to the buy rate
 
         public int IndexSell { set; get; } = 1; //which Regex.Match corresponds to the sell rate
 
+        public BankInfo(int ID, string bankName, string URI, string pattern, int indexBuy, int indexSell)
+        {
+            this.ID = ID;
+            this.BankName = bankName;
+            this.URI = URI;
+            this.Pattern = pattern;
+            this.IndexBuy = indexBuy;
+            this.IndexSell = indexSell;
+        }
     }
 }
