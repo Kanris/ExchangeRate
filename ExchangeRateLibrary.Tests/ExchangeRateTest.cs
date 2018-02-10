48e52a58c5af22db5bd6032d812b270c8aeddee9
Alexey Glukhov <kanris2@gmail.com>
2018-02-10 17:53:40 +0200

    Add Test Project for ExchangeRateLibrary

---
+++ ExchangeRateLibrary.Tests/ExchangeRateTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExchangeRateLibrary.Tests
+{
+    //Tests for ExchangeRate class
+    [TestClass]
+    public class ExchangeRateTest
+    {
+
+        [ClassInitialize]
+        public void InitializeClass(TestContext testContext)
+        {
+
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ ExchangeRateLibrary.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ExchangeRateLibrary.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ExchangeRateLibrary.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0eb59764-c5aa-43a8-ab45-778300cab15f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
