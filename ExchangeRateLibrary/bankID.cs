43ccbac262370603674067b2a77e78cb1e500c43
Alexey Glukhov <kanris2@gmail.com>
2018-02-12 08:40:42 +0200

    Add BankID class

---
+++ ExchangeRateLibrary/bankID.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeRateLibrary
+{
+    public class BankID
+    {
+        private readonly int id;
+
+        private BankID(int id)
+        {
+            this.id = id;
+        }
+
+        public static BankID Create(int id)
+        {
+            if (id < 0)
+                throw new Exception($"Bank id ({id}) must me greater or equal to zero");
+
+            return new BankID(id);
+        }
+
+        public static implicit operator int(BankID bankID)
+            => bankID.id;
+
+        public override string ToString()
+            => this.id.ToString();
+
+        public override bool Equals(object obj)
+        {
+            var bankID = obj as BankID;
+
+            if (ReferenceEquals(obj, null))
+                return false;
+
+            return bankID.id == this.id;
+        }
+
+        public override int GetHashCode()
+        {
+            return id.GetHashCode();
+        }
+    }
+}
